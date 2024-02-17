a3031c0651e65d74c576de8e519eeacfe36af580
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-02-18 04:18:17 +0530

    migrations

--- hello.py
+++ hello.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask import flash
+from flask import flash, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 
 
 
@@ -25,6 +26,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/o
 app.config['SECRET_KEY'] = "my super secret key"
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 if __name__ == '__main__':
     db.create_all()  
     app.run(debug=True)
@@ -35,6 +37,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(200), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -44,11 +47,35 @@ class Users(db.Model):
 class UserForm(FlaskForm):
     name = StringField("name", validators=[DataRequired()])
     email = StringField("email", validators=[DataRequired()])
+    favorite_color = StringField("favorite_color")
     submit = SubmitField("Submit")
+    
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        try:
+            db.session.commit()
+            flash("User Updated")
+            return render_template('update.html', form=form, name_to_update=name_to_update)
+        except Exception as e:
+            flash("Proble Error")
+    else:
+        return render_template('update.html', form=form, name_to_update=name_to_update)
 
+            
+                    
+        
 class NameForm(FlaskForm):
     name = StringField("What's your name", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+
 
 
 @app.route('/user/add', methods=['GET','POST'])
@@ -58,17 +85,16 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash("User added Successfully")
     our_users = Users.query.order_by(Users.date_added)
     return render_template('add_user.html', form=form, name=name, our_users=our_users)
-    
-
 
 @app.route('/')
 
