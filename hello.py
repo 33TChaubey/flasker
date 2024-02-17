b38663c062cdb765f31be79f4be272201cd12467
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-02-17 23:36:06 +0530

    secret key

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
-
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 # Create a FLask instance
 
 app = Flask(__name__)
 #Create a new route decorator
+#Create a form class
+app.config['SECRET_KEY'] = "my super secret key"
+class NameForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 @app.route('/')
 
@@ -30,3 +37,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
